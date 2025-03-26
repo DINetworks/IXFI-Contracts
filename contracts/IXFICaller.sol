e3173ab76b6a6ba5a451aa0d9f28074a4140af9c
0xawang <tik594746@gmail.com>
2025-03-26 05:16:57 -0700

    feature: implement IXFI token & multicall

---
+++ contracts/IXFICaller.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract IXFICaller {
+
+    struct CallData {
+        address target;
+        bytes4 selector;
+        bytes params;
+    }
+
+    error CallDataInvalid();
+
+    function multicall(bytes calldata callData) external {
+        CallData[] memory data = abi.decode(callData, (CallData[]));
+        uint256 call_count = data.length;
+        for (uint256 i = 0; i < call_count; ) {
+            (bool success, ) = data[i].target.call(abi.encodeWithSelector(data[i].selector, data[i].params));
+            if (!success) {
+                revert CallDataInvalid();
+            }
+
+            unchecked {
+                ++i;
+            }
+        }
+
+    }
+}
