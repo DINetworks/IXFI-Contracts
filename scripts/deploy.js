292f31ab7a90f33dca6b38fe0b9f5e826275fa1e
0xawang <tik594746@gmail.com>
2025-03-28 01:27:18 -0700

    update: deploy contracts to crossfi testnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,9 @@
 const dotenv = require('dotenv');
 const { ContractFactory, Wallet, JsonRpcProvider, keccak256, toUtf8Bytes } = require('ethers');
-const artifacts = require('../artifacts/contracts/IXFIGateway.sol/IXFIGateway.json')
+const artifactsIXFIGateway = require('../artifacts/contracts/IXFIGateway.sol/IXFIGateway.json')
+const artifactsGasRelayerXFI = require('../artifacts/contracts/GasRelayerXFI.sol/GasRelayerXFI.json')
+const artifactsIXFICaller = require('../artifacts/contracts/IXFICaller.sol/IXFICaller.json')
+const artifactsIXFI = require('../artifacts/contracts/IXFI.sol/IXFI.json')
 
 dotenv.config()
 
@@ -9,14 +12,30 @@ const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
 
 async function main() {
     // Get the contract factory for the contract
-    const IXFIGatewayFactory = new ContractFactory(artifacts.abi, artifacts.bytecode, wallet);
+    const IXFIGatewayFactory = new ContractFactory(artifactsIXFIGateway.abi, artifactsIXFIGateway.bytecode, wallet);
+    const gatewayContract = await IXFIGatewayFactory.deploy(wallet.address);
+    await gatewayContract.waitForDeployment();        
+    console.log(artifactsIXFIGateway.contractName, gatewayContract.target)
 
-    // Deploy the contract
-    const contract = await IXFIGatewayFactory.deploy(wallet.address);
-    console.log('deployer', wallet.address)
+    // IXFICaller Contract
+    const IXFICallerFactory = new ContractFactory(artifactsIXFICaller.abi, artifactsIXFICaller.bytecode, wallet);
+    const ixficallerContract = await IXFICallerFactory.deploy();
+    await ixficallerContract.waitForDeployment();
+    console.log(artifactsIXFICaller.contractName, ixficallerContract.target)
 
-    await contract.waitForDeployment();        
-    console.log(artifacts.contractName, contract.target)
+    // IXFI Token Contract
+    const IXFIFactory = new ContractFactory(artifactsIXFI.abi, artifactsIXFI.bytecode, wallet);
+    const ixfiContract = await IXFIFactory.deploy(wallet.address, "0xdaF0CEf4fc5447a5911b73C1b8148a6f838403D9");
+    await ixfiContract.waitForDeployment();
+    console.log(artifactsIXFI.contractName, ixfiContract.target)
+
+
+    // GasRelayererFactory
+    const GasRelayerXFIFactory = new ContractFactory(artifactsGasRelayerXFI.abi, artifactsGasRelayerXFI.bytecode, wallet);
+    const gRelayXFIcontract = await GasRelayerXFIFactory.deploy(ixfiContract.target);
+    await gRelayXFIcontract.waitForDeployment();        
+    console.log(artifactsGasRelayerXFI.contractName, gRelayXFIcontract.target)
+
 }
 
 main()
